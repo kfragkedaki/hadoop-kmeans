1643bc70858b802af94464a4442c8ccb91aec0b7
Vaggelis-Malandrakis <emalandrakis@dmst.aueb.gr>
2019-04-07 22:29:50 +0300

    Update printer.py
    Add comments and organize code

--- src/MapReduce/printer.py
+++ src/MapReduce/printer.py
@@ -1,22 +1,23 @@
-import random
 import matplotlib.pyplot as plt
 from scipy.spatial import distance
 
 centroids=[]
 
-x_dic={}
-m_list=[[],[],[]]
+# initiliaze list for points
 X=[[[],[]],[[],[]],[[],[]]]
+# initiliaze list for centroids
 M=[[],[]]
 
-print('Reading centroids...')
-
+# import centroids.txt file
 filepath = 'centroids.txt'
 with open(filepath) as fp:
+    # read line by line
     line = fp.readline()
     while line:
         if line:
+            # delete blanks for each lines
             line = line.strip()
+            # extract centroids coordinates
             cord = line.split(', ')
             centroids.append((float(cord[0]), float(cord[1])))
             M[0].append((float(cord[0])))
@@ -27,19 +28,20 @@ with open(filepath) as fp:
 
 fp.close()
 
-print('Reading centroids OK')
-print('Reading dataset...')
-
-#i = 0
-
+# import dataset.txt file
 filepath = 'dataset.txt'
 with open(filepath) as fp:
+    # read line by line
     line = fp.readline()
     while line:
         if line:
+            # delete blanks for each lines
             line = line.strip()
+            # extract points coordinates
             cord = line.split(',')
             x = (float(cord[0]), float(cord[1]))
+            # implement k means loop
+            # find the nearest centroid
             dist = 100000000000000
             selected_m = -1
             for m in centroids:
@@ -48,22 +50,21 @@ with open(filepath) as fp:
                     dist = test_distance
                     selected_m = centroids.index(m)
 
-            x_dic[x] = [selected_m, dist]
-            m_list[selected_m].append(x)
             X[selected_m][0].append(x[0])
             X[selected_m][1].append(x[1])
         else:
             break
         line = fp.readline()
-        i += 1
-        #if i % 50000 == 0:
-        #    print(i)
 
 fp.close()
 
+# print cluster 0 with red color
 plt.plot(X[0][0], X[0][1], 'ro')
+# print cluster 1 with green color
 plt.plot(X[1][0], X[1][1], 'go')
+# print cluster 2 with blue color
 plt.plot(X[2][0], X[2][1], 'bo')
+# print centroids with yellow color
 plt.plot(M[0], M[1], 'yo')
 plt.axis([-22, 20, -50, 40])
 plt.show()
